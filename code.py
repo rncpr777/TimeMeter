5a443a372684e7de66b98bb5fc82470c1d21be23
rncpr777 <95543255+rncpr777@users.noreply.github.com>
2023-06-08 23:33:48 +0300

    Update 1.1.
    Added days, extended maximum time to infinity, also fixed "if's tabulation in cicle

--- code.py
+++ code.py
@@ -3,7 +3,8 @@ import time
 seconds = '00'
 minutes = '00'
 hours = '00'
-for i in range(60 *  60 * 24):
+days = '00'
+while True:
     seconds = str(int(seconds) + 1)
     if int(seconds) < 10:
         seconds =  '0' + seconds
@@ -12,10 +13,13 @@ for i in range(60 *  60 * 24):
         if int(minutes) < 10:
             minutes =  '0' + minutes
         seconds = '00'
-        if minutes == '60':
-            minutes = '00'
-            hours = str(int(hours) + 1)
-            if int(hours) < 10:
-                hours = '0' + hours
-    print(hours + ':' + minutes + ':' + seconds)
+    if minutes == '60':
+        minutes = '00'
+        hours = str(int(hours) + 1)
+        if int(hours) < 10:
+            hours = '0' + hours
+    if hours == '24':
+        hours = '00'
+        days = str(int(days) + 1)
+    print(days + ':' + hours + ':' + minutes + ':' + seconds)
     time.sleep(1)
